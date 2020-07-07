e955ca8abdeb62ce344d3a5f687a6a12333302ae
Abe <abhi.rawat456@gmail.com>
2020-07-07 16:56:57 +0530

    Integrated graphql with the app

--- server/app.js
+++ server/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+
 const app = express();
+
+app.use("/graphql", graphqlHTTP({}));
 
 app.listen(3000, () => {
   console.log("Server started at 3000");
