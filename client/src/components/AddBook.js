8df009e5288cb437bcc8421c32e14da5c80150a8
Abe <abhi.rawat456@gmail.com>
2020-07-07 20:37:17 +0530

    Refactored queries to external file

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,15 +1,7 @@
 import React from "react";
 import { gql } from "apollo-boost";
 import { graphql } from "react-apollo";
-
-const getAuthorsQuery = gql`
-  {
-    authors {
-      name
-      id
-    }
-  }
-`;
+import { getAuthorsQuery } from "../queries/queries";
 
 function AddBook(props) {
   const displayAuthors = () => {
