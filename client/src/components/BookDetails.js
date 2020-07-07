d05eb429e6fe0f222ba1f9227beec721512e6493
Abe <abhi.rawat456@gmail.com>
2020-07-07 23:06:09 +0530

    Added styling

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -9,9 +9,15 @@ const BookDetails = (props) => {
       return (
         <div>
           <h2>{book.name}</h2>
-          <p>{book.genre}</p>
-          <p>{book.author.name}</p>
-          <p>All books by this author</p>
+          <p>
+            <strong>Genre :</strong> {book.genre}
+          </p>
+          <p>
+            <strong>Author :</strong> {book.author.name}
+          </p>
+          <p>
+            <strong>All books by this author</strong>
+          </p>
           <ul className="other-books">
             {book.author.books.map((book) => {
               return <li key={book.id}>{book.name}</li>;
