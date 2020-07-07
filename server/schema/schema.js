6169a533746440f7eeca9afe08272876f29b7f35
Abe <abhi.rawat456@gmail.com>
2020-07-07 18:27:01 +0530

    Added mutation for adding books

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -19,6 +19,7 @@ const BookType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    authorId: { type: GraphQLID },
     author: {
       type: AuthorType,
       resolve(parent, args) {},
@@ -78,6 +79,23 @@ const Mutation = new GraphQLObjectType({
           age: args.age,
         });
         return author.save();
+      },
+    },
+    addBook: {
+      type: BookType,
+      args: {
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString },
+        authorId: { type: GraphQLID },
+      },
+      resolve(parent, args) {
+        let book = new Book({
+          name: args.name,
+          genre: args.genre,
+          authorId: args.authorId,
+        });
+
+        return book.save();
       },
     },
   },
